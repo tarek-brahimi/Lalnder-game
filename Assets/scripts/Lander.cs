26c77ac2e46ad09e4bbd47f6e3fea4aeaaf60a08
tarek-brahimi <tarekbrahimi98@gmail.com>
2026-03-23 17:14:44 +0100

    congig the physique of the lander

--- Assets/scripts/Lander.cs
+++ Assets/scripts/Lander.cs
@@ -3,30 +3,30 @@ using UnityEngine.InputSystem;
 
 public class Lander : MonoBehaviour
 {
- private Rigidbody2D landerRigidbody2D;
-  private void Awake()
-  {
-    landerRigidbody2D = GetComponent <Rigidbody2D>();
-  }
+    private Rigidbody2D landerRigidbody2D;
+    private void Awake()
+    {
+        landerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
 
-  private void FixedUpdate()
-  {
+    private void FixedUpdate()
+    {
         if (Keyboard.current.upArrowKey.isPressed)
         {
-            
+            float force = 700f;
+            landerRigidbody2D.AddForce(force*transform.up * Time.deltaTime);
         }
-         if (Keyboard.current.downArrowKey.isPressed)
+
+        if (Keyboard.current.rightArrowKey.isPressed)
         {
-           
+            float  turnSpeeed=-100f;
+            landerRigidbody2D.AddTorque(turnSpeeed* Time.deltaTime);
         }
-         if (Keyboard.current.rightArrowKey.isPressed)
+        if (Keyboard.current.leftArrowKey.isPressed)
         {
-          
+             float  turnSpeeed=+100f;
+        landerRigidbody2D.AddTorque(turnSpeeed * Time.deltaTime);  
         }
-         if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            
-        }
-  }
- 
+    }
+
 }
